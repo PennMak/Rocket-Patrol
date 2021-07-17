7c693893c139d2e6cee7e48aac2be92fae92b1ed
“PennMak” <tsmak@ucsc.edu>
2021-07-17 14:30:45 +0800

    Update Play.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -19,7 +19,7 @@ class Play extends Phaser.Scene {
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
 
         // green UI background
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 'images').setOrigin(0, 0);
         // white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xC0C0C0).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xC0C0C0).setOrigin(0, 0);
